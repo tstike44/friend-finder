667b589de07a0aaa335acb8ba4598a9e4c7ee240
tstike44 <you@example.com>
2019-04-16 22:14:38 -0700

    created survey html page

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,69 @@
+var users = [
+    {
+        name: "Liz B",
+        photo: "https://vignette.wikia.nocookie.net/a-tv-challenge/images/f/ff/Liz_B.jpg/revision/latest?cb=20190131112852",
+        scores: ["5", "4", "5", "2", "4", "3", "3", "1", "4", "1",
+        ]
+    },
+    {
+        name: "Liz G",
+        photo: "https://comedycentral.mtvnimages.com/images/shows/kroll_show/video/ks_108_PubLIZity_preview_640x360.jpg",
+        scores: ["3", "3", "2", "5", "5", "5", "1", "1", "1", "1",
+        ]
+    },
+    {
+        name: "Gene Creemers",
+        photo: "https://uproxx.files.wordpress.com/2015/03/gene-creemers-show-us-song.jpg?w=650",
+        scores: ["5", "5", "5", "1", "1", "3", "3", "1", "4", "1",
+        ]
+    },
+    {
+        name: "Farley",
+        photo: "https://deecrowseer.files.wordpress.com/2014/03/krollshow-cp01.jpg",
+        scores: ["2", "5", "3", "2", "2", "2", "2", "1", "4", "1",
+        ]
+    },
+    {
+        name: "Nash Rickey",
+        photo: "https://media.pitchfork.com/photos/592c609feb335119a49f14eb/2:1/w_648/4fb920c1.jpg",
+        scores: ["5", "4", "5", "5", "5", "5", "5", "5", "1", "1",
+        ]
+    },
+    {
+        name: "Gil Faizon",
+        photo: "https://vignette.wikia.nocookie.net/comedybangbang/images/9/93/Faizon.jpeg/revision/latest?cb=20131009201859",
+        scores: ["5", "5", "5", "5", "5", "1", "1", "2", "4", "1",
+        ]
+    },
+    {
+        name: "George St. Geegland",
+        photo: "https://pbs.twimg.com/profile_images/530054996423749632/uIa2HxTV.jpeg",
+        scores: ["1", "2", "5", "1", "3", "5", "2", "2", "4", "2",
+        ]
+    },
+    {
+        name: "Niece Denise",
+        photo: "https://66.media.tumblr.com/8f743979c9f571c116db97add388e853/tumblr_n30rfaADzm1qzdv20o1_640.jpg",
+        scores: ["4", "4", "4", "3", "3", "3", "3", "1", "2", "1",
+        ]
+    },
+    {
+        name: "Bobby Bottleservice",
+        photo: "https://img.sharetv.com/shows/characters/large/kroll_show.bobby_bottleservice.jpg",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "5",
+        ]
+    },
+    {
+        name: "Ref Jeff",
+        photo: "https://img.sharetv.com/shows/characters/large/kroll_show.ref_jeff.jpg",
+        scores: ["2", "2", "2", "2", "2", "3", "3", "3", "3", "3",
+        ]
+    },
+    {
+        name: "C-Czar",
+        photo: "https://i.vimeocdn.com/video/489532996.webp?mw=900&mh=506",
+        scores: ["4", "2", "4", "2", "4", "2", "4", "2", "4", "2",
+        ]
+    },
+
+]
---
+++ FriendFinder/server.js
@@ -0,0 +1,10 @@
+//declare needed packages
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
