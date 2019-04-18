0aaddc51ccc1d3811b473dfc3b2baeaa7701479e
tstike44 <you@example.com>
2019-04-17 19:43:50 -0700

    asd

---
+++ server.js
@@ -0,0 +1,23 @@
+//declare needed packages
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+  });
+
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+  });
+
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+
+  
